cac7da08a9dbad1db184e1b1872f62aa541bf1e9
dimaematveev <dimaematveev@gmail.com>
2020-06-17 09:48:56 +0300

    Интерполяционный поиск готов

---
+++ Search.Console/Search.Console/Count1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Search.Console
+{
+    class Count1
+    {
+        
+
+        public  int ItemSearch { get;}
+        public int IndexItem { get;}
+        public int CountCompare { get; }
+
+        public Count1(int itemSearch, int indexItem, int countCompare)
+        {
+            ItemSearch = itemSearch;
+            IndexItem = indexItem;
+            CountCompare = countCompare;
+        }
+
+    }
+}
